bc694138b188c2792faab7a3356fe9aab638be0a
zaharich09 <97597336+zaharichswiftly@users.noreply.github.com>
2022-12-16 04:11:52 +0300

    flutter web ui and drag and drop functionality

---
+++ frontend/xmasfront/lib/components/dropzone_widget.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dropzone/flutter_dropzone.dart';
+import 'class/dropped_file.dart';
+
+class DropzoneWidget extends StatefulWidget {
+  final ValueChanged<DroppedFile> onDroppedFile;
+
+  const DropzoneWidget({Key? key, required this.onDroppedFile})
+      : super(key: key);
+
+  @override
+  State<DropzoneWidget> createState() => _DropzoneWidgetState();
+}
+
+class _DropzoneWidgetState extends State<DropzoneWidget> {
+  late DropzoneViewController controller;
+  bool isHighlighted = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorBackground = isHighlighted ? Colors.blue[300] : Colors.white;
+    final ColorButton = isHighlighted ? Colors.blue[100] : Colors.white;
+    final ColorBorder =
+        isHighlighted ? Colors.black : Color.fromARGB(255, 3, 210, 200);
+    return Container(
+      height: 300,
+      width: 400,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: ColorBackground,
+      ),
+      child: Stack(
+        children: [
+          DropzoneView(
+            onCreated: (controller) => this.controller = controller,
+            onHover: () => setState(() => isHighlighted = true),
+            onLeave: () => setState(() => isHighlighted = false),
+            onDrop: acceptFile,
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Icon(Icons.cloud_upload, size: 80, color: Colors.black),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Перетащите файл сюда",
+                  style: TextStyle(
+                    fontFamily: "Gilroy",
+                    fontWeight: FontWeight.w500,
+                    fontSize: 24,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                OutlinedButton.icon(
+                  onPressed: () async {
+                    final events = await controller.pickFiles();
+                    if (events.isEmpty) return;
+
+                    acceptFile(events.first);
+                  },
+                  icon: const Icon(
+                    Icons.search,
+                    size: 32,
+                  ),
+                  label: const Text(
+                    'Выберите файл',
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontFamily: "Gilroy",
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    elevation: 4.0,
+                    foregroundColor: Colors.black,
+                    backgroundColor: ColorButton,
+                    minimumSize: const Size(300, 60),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8)),
+                    side: BorderSide(
+                      width: 3,
+                      color: ColorBorder,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future acceptFile(dynamic event) async {
+    final name = event.name;
+    final mime = await controller.getFileMIME(event);
+    final bytes = await controller.getFileSize(event);
+    final url = await controller.createFileUrl(event);
+
+    print('File name: $name');
+    print('File mime: $mime');
+    print('File size: $bytes');
+    print('File url: $url');
+
+    final droppedFile = DroppedFile(
+      name: name,
+      bytes: bytes,
+      mime: mime,
+      url: url,
+    );
+
+    widget.onDroppedFile(droppedFile);
+    setState(() => isHighlighted = false);
+  }
+}
---
+++ frontend/xmasfront/lib/pages/Home_page.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:xmasfront/components/class/dropped_file.dart';
+import 'package:xmasfront/components/dropped_file_widget.dart';
+import 'package:xmasfront/components/dropzone_widget.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  DroppedFile? file;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          Column(
+            children: [
+              const SizedBox(height: 60),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50),
+                child: Column(
+                  // ignore: prefer_const_literals_to_create_immutables
+                  children: [
+                    const Center(
+                      child: Text(
+                        "M I S I S    A T L A N T I S",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontFamily: 'Gilroy',
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+              Material(
+                elevation: 4.0,
+                borderRadius: BorderRadius.circular(20),
+                child: SizedBox(
+                  height: 200,
+                  width: 400,
+                  child: Container(
+                    alignment: const Alignment(0, -0.75),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const Text(
+                          "Добро пожаловать!",
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontFamily: 'Gilroy',
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 15),
+                          child: Text(
+                            "Для начала работы импортируйте файл типа doc, docx, pdf",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontFamily: 'Gilroy',
+                              fontWeight: FontWeight.w400,
+                              color: Color(0xFF8F8F8F),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Material(
+                elevation: 4.0,
+                borderRadius: BorderRadius.circular(20),
+                child: DropzoneWidget(
+                    onDroppedFile: (file) => setState(() => this.file = file)),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              DroppedFileWidget(
+                file: file,
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
