b24b4e24fcf312d25671714ccc153ec079811e79
hho <khanipovm@gmail.com>
2022-12-16 16:58:16 +0300

    Updated back

--- backend/routes/router.py
+++ backend/routes/router.py
@@ -1,24 +1,22 @@
 import os
-from fastapi import APIRouter, File, UploadFile, Response
+from fastapi import APIRouter, File, UploadFile, Request, Response, HTTPException
 from config import DATA_PATH
 
 
 router = APIRouter(prefix="/api")
 
 @router.post("/upload")
-async def upload(files: list[UploadFile] = File(...)):
-    for file in files:
-        try:
-            contents = file.file.read()
-            file_path = os.path.join(DATA_PATH, file.filename)
-            with open(file_path, 'wb') as f:
-                f.write(contents)
-        except Exception:
-            return {"message": "There was an error uploading the file(s)"}
-        finally:
-            file.file.close()
-
-    return {"message": f"Successfuly uploaded {[file.filename for file in files]}"}
+async def upload(file: UploadFile = File(...)):
+    filename = file.filename
+    ext = filename[filename.rfind('.') + 1:-1]
+    if ext not in ["doc", "docx", "rtf", "pdf"]:
+        raise HTTPException(400, detail="Invalid document type")
+    contents = file.file.read()
+    file_path = os.path.join(DATA_PATH, file.filename)
+    with open(file_path, 'wb') as f:
+        f.write(contents)
+    file.file.close()
+    return {"message": f"Successfuly uploaded {file.filename}"}
 
 
 @router.get("/download")
